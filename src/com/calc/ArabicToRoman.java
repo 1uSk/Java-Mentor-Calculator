c52291a9e7b0c78fb617a3e377071e4939e8a8c1
1uSk <gg-luck@rambler.ru>
2020-11-10 03:21:06 +0300

    improvements

--- src/com/calc/ArabicToRoman.java
+++ src/com/calc/ArabicToRoman.java
@@ -2,21 +2,24 @@ package com.calc;
 
 public class ArabicToRoman {
     static final int min = 1;
-    static final int max = 90;
-    static final String[] RomanXXX = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    static final String[] RomanX = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    static final int max = 100;
+    static final String[] RomanI = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    static final String[] RomanX = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    static final String[] RomanC = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+
 
     public String arabicToRoman(int arabic){
 
         if(arabic < min || arabic >= max){
-            throw new IllegalArgumentException("Значение должно быть от 1 до 90");
+            throw new IllegalArgumentException("Значение должно быть от 1 до 100");
         }
 
         String result = "";
 
         StringBuilder sb = new StringBuilder();
-        sb.append(RomanXXX[arabic % 100 / 10]);
-        sb.append(RomanX[arabic % 10]);
+        sb.append(RomanC[arabic % 1000 / 100]);
+        sb.append(RomanX[arabic % 100 / 10]);
+        sb.append(RomanI[arabic % 10]);
 
         result = sb.toString();
 
