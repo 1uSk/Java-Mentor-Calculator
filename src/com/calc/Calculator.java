7e532a7abf852e5da3effc52e1f0febcdce05a83
1uSk <gg-luck@rambler.ru>
2020-11-10 03:37:25 +0300

    improvements

--- src/com/calc/Calculator.java
+++ src/com/calc/Calculator.java
@@ -8,12 +8,16 @@ public class Calculator {
             RomanToArabic rta = new RomanToArabic();
             result = calc( rta.romanToArabic(firstOp), rta.romanToArabic(secondOp), operation );
         } catch (Exception e){
+            throw new IllegalArgumentException(e.getMessage());
         }
         return result;
     }
 
     public int calc(int firstOp, int secondOp, String operation){
         int result = 0;
+
+        if (firstOp > 10 || secondOp > 10){ throw new IllegalArgumentException("Аргумент не может быть больше 10"); }
+
         switch (operation){
             case "+":
                 result = firstOp + secondOp;
